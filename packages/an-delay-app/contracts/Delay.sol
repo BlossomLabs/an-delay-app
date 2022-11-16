f6f7c71f23b0daa741c953e3fe1264f58b8f897a
Sem <931684+sembrestels@users.noreply.github.com>
2022-11-15 18:41:18 -0600

    Migrate to hardhat and add forwarder fee in contract

--- packages/an-delay-app/contracts/Delay.sol
+++ packages/an-delay-app/contracts/Delay.sol
@@ -2,17 +2,23 @@ pragma solidity ^0.4.24;
 
 import "@aragon/os/contracts/apps/AragonApp.sol";
 import "@aragon/os/contracts/common/IForwarder.sol";
+import "@aragon/os/contracts/common/IForwarderFee.sol";
 import "@aragon/os/contracts/lib/math/SafeMath64.sol";
+import "@aragon/os/contracts/common/SafeERC20.sol";
+import "@aragon/os/contracts/lib/token/ERC20.sol";
 
 
-contract Delay is AragonApp, IForwarder {
+contract Delay is AragonApp, IForwarder, IForwarderFee {
     using SafeMath64 for uint64;
+    using SafeERC20 for ERC20;
 
     bytes32 public constant SET_DELAY_ROLE = keccak256("SET_DELAY_ROLE");
     bytes32 public constant DELAY_EXECUTION_ROLE = keccak256("DELAY_EXECUTION_ROLE");
     bytes32 public constant PAUSE_EXECUTION_ROLE = keccak256("PAUSE_EXECUTION_ROLE");
     bytes32 public constant RESUME_EXECUTION_ROLE = keccak256("RESUME_EXECUTION_ROLE");
     bytes32 public constant CANCEL_EXECUTION_ROLE = keccak256("CANCEL_EXECUTION_ROLE");
+    bytes32 public constant CHANGE_AMOUNT_ROLE = keccak256("CHANGE_AMOUNT_ROLE");
+    bytes32 public constant CHANGE_DESTINATION_ROLE = keccak256("CHANGE_DESTINATION_ROLE");
 
     string private constant ERROR_NO_SCRIPT = "DELAY_NO_SCRIPT";
     string private constant ERROR_CAN_NOT_EXECUTE = "DELAY_CAN_NOT_EXECUTE";
@@ -20,16 +26,25 @@ contract Delay is AragonApp, IForwarder {
     string private constant ERROR_SCRIPT_EXECUTION_PASSED = "DELAY_SCRIPT_EXECUTION_PASSED";
     string private constant ERROR_CAN_NOT_RESUME = "DELAY_CAN_NOT_RESUME";
     string private constant ERROR_CAN_NOT_FORWARD = "DELAY_CAN_NOT_FORWARD";
+    string private constant ERROR_INVALID_FEE_TOKEN = "DELAY_INVALID_FEE_TOKEN";
+    string private constant ERROR_INVALID_FEE_AMOUNT = "DELAY_INVALID_FEE_AMOUNT";
+    string private constant ERROR_INVALID_FEE_DESTINATION = "DELAY_INVALID_FEE_DESTINATION";
+    string private constant ERROR_FEE_TRANSFER_REVERTED = "DELAY_FEE_TRANSFER_REVERTED";
 
     struct DelayedScript {
         uint64 executionTime;
         uint64 pausedAt;
+        address sender;
+        uint256 feeAmount;
         bytes evmCallScript;
     }
 
     uint64 public executionDelay;
     uint256 public delayedScriptsNewIndex = 0;
     mapping(uint256 => DelayedScript) public delayedScripts;
+    ERC20 public feeToken;
+    uint256 public feeAmount;
+    address public feeDestination;
 
     event DelayedScriptStored(uint256 scriptId);
     event ExecutionDelaySet(uint64 executionDelay);
@@ -37,6 +52,8 @@ contract Delay is AragonApp, IForwarder {
     event ExecutionPaused(uint256 scriptId);
     event ExecutionResumed(uint256 scriptId);
     event ExecutionCancelled(uint256 scriptId);
+    event ChangeFeeAmount(uint256 previousAmount, uint256 newAmount);
+    event ChangeFeeDestination(address indexed previousDestination, address indexed newDestination);
 
     modifier scriptExists(uint256 _scriptId) {
         require(delayedScripts[_scriptId].executionTime != 0, ERROR_NO_SCRIPT);
@@ -44,12 +61,22 @@ contract Delay is AragonApp, IForwarder {
     }
 
     /**
-    * @notice Initialize the Delay app
+    * @notice Initialize the Delay app with a fee of `@tokenAmount(_feeToken, _feeAmount)`
     * @param _executionDelay The delay in seconds a user will have to wait before executing a script
+    * @param _feeToken ERC20 address for the fee token
+    * @param _feeAmount Amount of tokens collected as a fee on each forward
+    * @param _feeDestination Destination for collected fees
     */
-    function initialize(uint64 _executionDelay) external onlyInit {
+    function initialize(uint64 _executionDelay, ERC20 _feeToken, uint256 _feeAmount, address _feeDestination) external onlyInit {
         initialized();
+        require(_feeDestination != address(0), ERROR_INVALID_FEE_DESTINATION);
+        require(address(_feeToken) != address(0), ERROR_INVALID_FEE_TOKEN);
+
         executionDelay = _executionDelay;
+
+        feeToken = _feeToken;
+        feeAmount = _feeAmount;
+        feeDestination = _feeDestination;
     }
 
     /**
@@ -63,11 +90,41 @@ contract Delay is AragonApp, IForwarder {
     }
 
     /**
+    * @notice Change fee to `@tokenAmount(self.feeToken(): address, _feeAmount)`
+    * @param _feeAmount Fee amount
+    */
+    function changeFeeAmount(uint256 _feeAmount) external authP(CHANGE_AMOUNT_ROLE, arr(_feeAmount, feeAmount)) {
+        require(_feeAmount != feeAmount, ERROR_INVALID_FEE_AMOUNT);
+        emit ChangeFeeAmount(feeAmount, _feeAmount);
+        feeAmount = _feeAmount;
+    }
+
+    /**
+    * @notice Change fee destination to `_feeDestination`
+    * @param _feeDestination Destination for collected fees
+    */
+    function changeFeeDestination(address _feeDestination) external authP(CHANGE_DESTINATION_ROLE, arr(_feeDestination, feeDestination)) {
+        require(_feeDestination != feeDestination && _feeDestination != address(0), ERROR_INVALID_FEE_DESTINATION);
+        emit ChangeFeeDestination(feeDestination, _feeDestination);
+        feeDestination = _feeDestination;
+    }
+
+    /**
     * @notice Delays execution for `@transformTime(self.executionDelay(): uint)`
     * @param _evmCallScript The script that can be executed after a delay
     */
     function delayExecution(bytes _evmCallScript) external auth(DELAY_EXECUTION_ROLE) returns (uint256) {
         return _delayExecution(_evmCallScript);
+    }
+
+    /**
+    * @notice Tells the forward fee token and amount of the Delay app
+    * @dev IFeeForwarder interface conformance
+    * @return Forwarder fee token address
+    * @return Forwarder fee amount
+    */
+    function forwardFee() external view returns (address, uint256) {
+        return (address(feeToken), feeAmount);
     }
 
     function isForwarder() external pure returns (bool) {
@@ -108,6 +165,13 @@ contract Delay is AragonApp, IForwarder {
     * @param _delayedScriptId The ID of the script execution to cancel
     */
     function cancelExecution(uint256 _delayedScriptId) external scriptExists(_delayedScriptId) auth(CANCEL_EXECUTION_ROLE) {
+        uint256 amount = delayedScripts[_delayedScriptId].feeAmount;
+
+        // Don't do an unnecessary transfer if there was no fee
+        if (amount > 0) {
+          feeToken.transfer(feeDestination, amount);
+        }
+
         delete delayedScripts[_delayedScriptId];
 
         emit ExecutionCancelled(_delayedScriptId);
@@ -122,6 +186,11 @@ contract Delay is AragonApp, IForwarder {
 
         DelayedScript memory delayedScript = delayedScripts[_delayedScriptId];
         delete delayedScripts[_delayedScriptId];
+
+        // Don't do an unnecessary transfer if there was no fee
+        if (delayedScript.feeAmount > 0) {
+          feeToken.transfer(delayedScript.sender, delayedScript.feeAmount);
+        }
 
         runScript(delayedScript.evmCallScript, new bytes(0), new address[](0));
         emit ExecutedScript(_delayedScriptId);
@@ -156,10 +225,16 @@ contract Delay is AragonApp, IForwarder {
     }
 
     function _delayExecution(bytes _evmCallScript) internal returns (uint256) {
+
+        // Don't do an unnecessary transfer if there's no fee right now
+        if (feeAmount > 0) {
+            require(feeToken.safeTransferFrom(msg.sender, address(this), feeAmount), ERROR_FEE_TRANSFER_REVERTED);
+        }
+
         uint256 delayedScriptIndex = delayedScriptsNewIndex;
         delayedScriptsNewIndex++;
 
-        delayedScripts[delayedScriptIndex] = DelayedScript(getTimestamp64().add(executionDelay), 0, _evmCallScript);
+        delayedScripts[delayedScriptIndex] = DelayedScript(getTimestamp64().add(executionDelay), 0, msg.sender, feeAmount, _evmCallScript);
 
         emit DelayedScriptStored(delayedScriptIndex);
 
--- packages/an-delay-app/contracts/Imports.sol
+++ packages/an-delay-app/contracts/Imports.sol
@@ -1,5 +1,8 @@
 pragma solidity ^0.4.24;
 
 import "@aragon/apps-shared-minime/contracts/MiniMeToken.sol";
+import "@aragon/os/contracts/acl/ACL.sol";
+import "@aragon/os/contracts/kernel/Kernel.sol";
+import "@aragon/os/contracts/factory/DAOFactory.sol";
 
 contract Imports {}
